6a49fda3f3d690b99f474bad9f97ca141c0885cb
Sockalingam <74052417+Sockalingam29@users.noreply.github.com>
2022-11-01 21:26:13 +0530

    players restricted to 2

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ app.use(cors())
 
 const io = new Server(server,{
     cors: {
-        origin: "https://clever-genie-dcae66.netlify.app",
-        methods: ["GET", "POST"]
+        origin: "*",
     }
 });
 
@@ -18,22 +17,39 @@ app.get('/', (req, res) => {
   res.send("Chess backend is here!");
 });
 
+let player=[];
+
 io.on("connection", async (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
-  await socket.on("join_room", async(data) => {
-    console.log("Joined room")
-    await socket.join(data);
+  await socket.on("join_room", async(room) => {
+    await socket.join(room);
+    let numClients = player.length;
+    console.log("Num of clients"+numClients)
+    if(numClients === 0){
+      await socket.join(room);
+      player[0]=socket.id;
+      console.log(socket.id +" Player 1 joined")
+      await socket.emit("player", {player:"white"});
+    }
+    else if(numClients === 1){
+      await socket.join(room);
+      player[1]=socket.id;
+      console.log(socket.id +" Player 2 joined")
+      await socket.emit("player", {player:"black"});
+    }else{
+      await socket.emit("room_full", {room:room});
+    }
   });
 
   await socket.on("send_message", async(data) => {
-    console.log("Data from index js")
-    console.log(data)
     await socket.to(data.room).emit("receive_board", data);
+    socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
   });
 });
-
-const PORT = process.env.PORT || 5000;
+})
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => {
   console.log('listening on *:3001');
-});
+})
